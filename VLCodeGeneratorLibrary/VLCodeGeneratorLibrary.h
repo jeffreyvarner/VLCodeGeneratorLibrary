6f70025776ee8572ecfc3fe3c81bd8c9164a8a74
jeffreyvarner <jeff@curveos.com>
2013-10-02 11:37:56 -0400

    Initial Commit

---
+++ VLCodeGeneratorLibrary/VLCodeGeneratorLibrary.h
@@ -0,0 +1,13 @@
+//
+//  VLCodeGeneratorLibrary.h
+//  VLCodeGeneratorLibrary
+//
+//  Created by Jeffrey Varner on 10/2/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VLCodeGeneratorLibrary : NSObject
+
+@end
---
+++ VLCodeGeneratorLibrary/VLCodeGeneratorLibrary.m
@@ -0,0 +1,13 @@
+//
+//  VLCodeGeneratorLibrary.m
+//  VLCodeGeneratorLibrary
+//
+//  Created by Jeffrey Varner on 10/2/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import "VLCodeGeneratorLibrary.h"
+
+@implementation VLCodeGeneratorLibrary
+
+@end
